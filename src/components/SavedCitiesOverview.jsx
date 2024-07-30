3ebb7997816f0ef98afd4c9e4e75495b1825fa6b
Pia Koester <44154431+Pia-Koester@users.noreply.github.com>
2024-07-30 22:34:47 +0200

    feature: fetching all cities from API

--- src/components/SavedCitiesOverview.jsx
+++ src/components/SavedCitiesOverview.jsx
@@ -1,10 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function SavedCitiesOverview() {
+  const [cities, setCities] = useState([]); //wir brauchen ein Array mit allen Städten die wir von der API bekommen
+
+  useEffect(() => {
+    getCities(); //immer wenn sich etwas verändert, bzw. beim ersten Laden sollen die Städte abgefragt werden
+  }, []); //leerer dependencies array, weil wir hier nichts brauchen
+
+  const getCities = async () => {
+    //async, denn wir müssen auch eine Antwort warten und können nicht sagen wie lange, aber die restliche Seite darf weiterladen
+    try {
+      const result = await fetch("http://localhost:3000/cities"); //jetzt warten wir auf die Ergebnisse, wir fragen unsere API an, ob es was gibt
+      const json = await result.json(); // wir müssen die antwort in ein verwendbares Format packen JSON
+      console.log(json); //immer mal was loggen damit wir kontrolletti spielen können
+      setCities(json); // useState wird mit den Ergebnissen geupdated
+    } catch (error) {
+      console.log(error); //falls was schief geht
+    }
+  };
+
   return (
     <div className="saved_cities_container inconsolata-normal">
       <h3 className="cabin-bold">Saved Cities</h3>
-      <div>New York</div>
-      <div>Los Angeles</div>
-      <div>Chicago</div>
+      {cities.map((city) => {
+        //map über alle Elemente des Arrays. Einzelne Felder des JSON werden über Dot Notation abgefragt .name .uuid etc
+        return <div key={city.uuid}>{city.name}</div>; //key ist bei Listen IMMER ein erforderliches Property damit React sich nicht selbst verwirrt
+      })}
+
       <div>PAGINATION</div>
     </div>
   );
