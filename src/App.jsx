3492662657596971cc0397c7d3372018bd6d1843
Pia Koester <44154431+Pia-Koester@users.noreply.github.com>
2024-07-30 17:43:19 +0200

    background image and glass morphism for box

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import SavedCitiesOverview from "./components/SavedCitiesOverview";
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="main_container">
-        <Navbar />
-        <div className="city_selection_container inconsolata-normal">
-          <h1 className="cabin-bold">City Selection</h1>
-          <input type="text" />
-          <button>search city</button>
+        <div className="left">
+          <div className="city_selection_container inconsolata-normal">
+            <h1 className="cabin-bold">City Selection</h1>
+            <input type="text" />
+            <button>search city</button>
+          </div>
+          <SavedCitiesOverview />
         </div>
-        <div className="saved_cities_container inconsolata-normal">
-          <h3 className="cabin-bold">Saved Cities</h3>
-          <div>New York</div>
-          <div>Los Angeles</div>
-          <div>Chicago</div>
-          <div>PAGINATION</div>
-        </div>
-        <div className="city_preview_container inconsolata-normal">
+        <div className="city_preview_container inconsolata-normal right">
           <h3 className="cabin-bold">City Preview</h3>
           <p>City: New York</p>
           <p>Weather: Sunny</p>
---
+++ src/components/SavedCitiesOverview.jsx
@@ -0,0 +1,13 @@
+function SavedCitiesOverview() {
+  return (
+    <div className="saved_cities_container inconsolata-normal">
+      <h3 className="cabin-bold">Saved Cities</h3>
+      <div>New York</div>
+      <div>Los Angeles</div>
+      <div>Chicago</div>
+      <div>PAGINATION</div>
+    </div>
+  );
+}
+
+export default SavedCitiesOverview;
