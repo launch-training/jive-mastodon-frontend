3e049ffce08890ea830e09f917e8d5f0d2eea1bc
Pia Koester <44154431+Pia-Koester@users.noreply.github.com>
2024-08-07 10:24:54 +0200

    feat: alert after search preview users get shown an alert that they can refine their search closes #2

--- src/components/SelectCity.jsx
+++ src/components/SelectCity.jsx
@@ -1,31 +1,40 @@
-import {Input} from 'antd';
+import { Input } from "antd";
 import { useState } from "react";
 
 const { Search } = Input;
 
-const SelectCity = ({setCity}) => {
+const SelectCity = ({ setCity }) => {
+  const [onChangeCity, setOnChangeCity] = useState("");
+  const [toggleAlert, setToggleAlert] = useState(false);
+  console.log(onChangeCity);
 
-    const [onChangeCity, setOnChangeCity] = useState("");
+  const handleSearch = () => {
+    setCity(onChangeCity);
+    setToggleAlert((prev) => !prev);
+  };
 
-    const handleSearch = () => {
-        setCity(onChangeCity);
-    }
-
-    return (
-        <div className="city_selection_container inconsolata-normal">
-        <h1 className="cabin-bold major_headline">City Selection</h1>
-        <Search
+  return (
+    <div className="city_selection_container inconsolata-normal">
+      <h1 className="cabin-bold major_headline">City Selection</h1>
+      <Search
         placeholder="Enter city name"
         allowClear
         enterButton="Search city"
         size="medium"
-        value = {onChangeCity} 
-        onChange = {e => setOnChangeCity(e.target.value)}
+        value={onChangeCity}
+        onChange={(e) => setOnChangeCity(e.target.value)}
         onSearch={handleSearch}
-        />
+      />
+      {toggleAlert && (
+        <div>
+          <p className="alert_city_name">
+            Please refine your search if you are not happy with the result.
+            Otherwise click save city.
+          </p>
         </div>
-    );
-}
-    
+      )}
+    </div>
+  );
+};
 
 export default SelectCity;
